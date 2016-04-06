651990020ed26cba49a5d8641cb1ba39ae7dc45c
Takashi Kokubun <takashikkbn@gmail.com>
2016-04-07 01:18:47 +0900

    Fix tests to work

--- spec/libx11_spec.rb
+++ spec/libx11_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
-describe Libx11 do
-  it 'has a version number' do
-    expect(Libx11::VERSION).not_to be nil
+describe LibX11 do
+  describe '.xopen_display' do
+    it 'returns a display' do
+      display = LibX11.xopen_display('')
+      expect(display).to be_a(LibX11::Display)
+      LibX11.xclose_display(display)
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.xcreate_simple_window' do
+    it 'returns a window' do
+      display = LibX11.xopen_display('')
+      window = LibX11.xcreate_simple_window(
+        display, display.default_root_window,
+        100, 100, 320, 280, 2, 0, 0,
+      )
+      expect(window).to be_a(Integer)
+      LibX11.xdestroy_window(display, window)
+      LibX11.xclose_display(display)
+    end
   end
 end
