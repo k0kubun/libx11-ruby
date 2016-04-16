64680f6b63adb936c764d9065ae0fca5708bc01f
Takashi Kokubun <takashikkbn@gmail.com>
2016-04-16 12:46:34 +0900

    Drop explicit function declaration

--- example/window_manager.rb
+++ example/window_manager.rb
@@ -10,14 +10,12 @@ class XServer
 
   def self.bind_events(display, window, event_mask)
     wm_detected = false
-    LibX11::Xlib.XSetErrorHandler(
-      FFI::Function.new(:int, [LibX11::Xlib::Display.ptr, LibX11::Xlib::XErrorEvent.ptr]) do |display, error|
-        if error[:error_code] == LibX11::X::BadAccess
-          wm_detected = true
-        end
-        0
+    LibX11::Xlib.XSetErrorHandler -> (display, error) do
+      if error[:error_code] == LibX11::X::BadAccess
+        wm_detected = true
       end
-    )
+      0
+    end
 
     LibX11::Xlib.XSelectInput(display, window, event_mask)
     LibX11::Xlib.XSync(display, 0)
@@ -40,12 +38,10 @@ XServer.with_connection do |display|
     LibX11::X::SubstructureNotifyMask,
   )
 
-  LibX11::Xlib.XSetErrorHandler(
-    FFI::Function.new(:int, [LibX11::Xlib::Display.ptr, LibX11::Xlib::XErrorEvent.ptr]) do |display, error|
-      $stderr.puts "Error detected: (error_code=#{error[:error_code]})"
-      0
-    end
-  )
+  LibX11::Xlib.XSetErrorHandler -> (display, error) do
+    $stderr.puts "Error detected: (error_code=#{error[:error_code]})"
+    0
+  end
 
   loop do
     event = LibX11::Xlib::XEvent.new
