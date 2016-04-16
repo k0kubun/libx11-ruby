9196be4cec9d3735aa504625b8df0646f569b2a1
Takashi Kokubun <takashikkbn@gmail.com>
2016-04-16 12:10:36 +0900

    Add dropped functions

--- lib/libx11/xlib.rb
+++ lib/libx11/xlib.rb
@@ -5,11 +5,13 @@ module LibX11
     ffi_lib 'X11'
 
     typedef :int,   :Bool
+    typedef :uchar, :KeyCode
     typedef :ulong, :Atom
     typedef :ulong, :Time
     typedef :ulong, :XID
     typedef :XID,   :Colormap
     typedef :XID,   :Drawable
+    typedef :XID,   :KeySym
     typedef :XID,   :Window
 
     require 'libx11/xlib/display'
@@ -17,18 +19,24 @@ module LibX11
 
     callback :XErrorHandler, [Display.ptr, XErrorEvent.ptr], :int
 
-    attach_function :XOpenDisplay,        [:string], Display.ptr              # L1483
-    attach_function :XCreateSimpleWindow, [Display.ptr, :Window, :int, :int, :uint, :uint, :uint, :ulong, :ulong], :Window # L1621
-    attach_function :XDefaultRootWindow,  [Display.ptr], :Window              # L1769
-    attach_function :XBlackPixel,         [Display.ptr, :int], :ulong         # L1789
-    attach_function :XWhitePixel,         [Display.ptr, :int], :ulong         # L1793
-    attach_function :XSetErrorHandler,    [:XErrorHandler], :XErrorHandler    # L1843
-    attach_function :XCloseDisplay,       [Display.ptr], :int                 # L2160
-    attach_function :XDefaultScreen,      [Display.ptr], :int                 # L2227
-    attach_function :XDestroyWindow,      [Display.ptr, :Window], :int        # L2243
-    attach_function :XMapWindow,          [Display.ptr, :Window], :int        # L2806
-    attach_function :XNextEvent,          [Display.ptr, XEvent.ptr], :int, blocking: true # L2841
-    attach_function :XSelectInput,        [Display.ptr, :Window, :long], :int # L3129
-    attach_function :XSync,               [Display.ptr, :Bool], :int, blocking: true # L3409
+    attach_function :XOpenDisplay,        [:string], Display.ptr
+    attach_function :XCreateSimpleWindow, [Display.ptr, :Window, :int, :int, :uint, :uint, :uint, :ulong, :ulong], :Window
+    attach_function :XLookupKeysym,       [XKeyEvent.ptr, :int], :KeySym
+    attach_function :XDefaultRootWindow,  [Display.ptr], :Window
+    attach_function :XBlackPixel,         [Display.ptr, :int], :ulong
+    attach_function :XWhitePixel,         [Display.ptr, :int], :ulong
+    attach_function :XDisplayString,      [Display.ptr], :string
+    attach_function :XSetErrorHandler,    [:XErrorHandler], :XErrorHandler
+    attach_function :XCloseDisplay,       [Display.ptr], :int
+    attach_function :XConnectionNumber,   [Display.ptr], :int
+    attach_function :XDefaultScreen,      [Display.ptr], :int
+    attach_function :XDestroyWindow,      [Display.ptr, :Window], :int
+    attach_function :XGrabKey,            [Display.ptr, :int, :uint, :Window, :Bool, :int, :int], :int
+    attach_function :XKeysymToKeycode,    [Display.ptr, :KeySym], :KeyCode
+    attach_function :XMapWindow,          [Display.ptr, :Window], :int
+    attach_function :XNextEvent,          [Display.ptr, XEvent.ptr], :int, blocking: true
+    attach_function :XSelectInput,        [Display.ptr, :Window, :long], :int
+    attach_function :XSync,               [Display.ptr, :Bool], :int, blocking: true
+    attach_function :XUngrabKey,          [Display.ptr, :int, :uint, :Window], :int
   end
 end
